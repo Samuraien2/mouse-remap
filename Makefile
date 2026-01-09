5100a5ea92365a606b1edf120d48254167b81498
Samuraien2 <samuraien2@proton.me>
2026-01-09 21:03:57 +0100

    glow up

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ BIN = mouse-remap
 
 .PHONY: clean
 $(BIN):
-	cc main.c -o $@
+	cc main.c -o $@ -O2 -s
 
 clean:
 	rm $(BIN)
--- main.c
+++ main.c
@@ -1,34 +1,12 @@
 #include <fcntl.h>
-#include <linux/input.h>
 #include <linux/uinput.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
-#include <sys/time.h>
+#include <string.h>
 
 #define MOUSE_DEVICE "/dev/input/by-id/usb-Razer_Razer_DeathAdder_Elite-event-mouse"
 
-int uinput_fd;
-
-void emit(int fd, int type, int code, int val) {
-    struct input_event ev;
-    gettimeofday(&ev.time, NULL);
-    ev.type = type;
-    ev.code = code;
-    ev.value = val;
-    write(fd, &ev, sizeof(ev));
-}
-
-int setup_uinput_device() {
-    int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
-    if (fd < 0) {
-        perror("Opening /dev/uinput");
-        exit(4);
-    }
-
-    // Enable mouse and keyboard event types
+void setup_uinput(int fd) {
     ioctl(fd, UI_SET_EVBIT, EV_KEY);
     ioctl(fd, UI_SET_KEYBIT, KEY_8);
     ioctl(fd, UI_SET_KEYBIT, KEY_9);
@@ -44,9 +22,8 @@ int setup_uinput_device() {
 
     ioctl(fd, UI_SET_EVBIT, EV_SYN);
 
-    struct uinput_setup usetup;
-    memset(&usetup, 0, sizeof(usetup));
-    snprintf(usetup.name, UINPUT_MAX_NAME_SIZE, "VirtualMouseKeyboard");
+    struct uinput_setup usetup = {0};
+    strcpy(usetup.name, "MCMouse");
     usetup.id.bustype = BUS_USB;
     usetup.id.vendor = 0x1234;
     usetup.id.product = 0x5678;
@@ -54,16 +31,6 @@ int setup_uinput_device() {
 
     ioctl(fd, UI_DEV_SETUP, &usetup);
     ioctl(fd, UI_DEV_CREATE);
-    sleep(1); // Give system time to register
-
-    return fd;
-}
-
-void send_key(int fd, int keycode) {
-    emit(fd, EV_KEY, keycode, 1);
-    emit(fd, EV_SYN, SYN_REPORT, 0);
-    emit(fd, EV_KEY, keycode, 0);
-    emit(fd, EV_SYN, SYN_REPORT, 0);
 }
 
 int main() {
@@ -78,29 +45,26 @@ int main() {
         return 2;
     }
 
-    uinput_fd = setup_uinput_device();
-
-    struct input_event ev;
-    while (read(fd, &ev, sizeof(ev)) > 0) {
-        if (ev.type == EV_KEY) {
-            if (ev.code == BTN_EXTRA) {
-                if (ev.value == 1) send_key(uinput_fd, KEY_8);
-            } else if (ev.code == BTN_SIDE) {
-                if (ev.value == 1) send_key(uinput_fd, KEY_9);
-            } else {
-                emit(uinput_fd, EV_KEY, ev.code, ev.value);
-                emit(uinput_fd, EV_SYN, SYN_REPORT, 0);
-            }
-        } else if (ev.type == EV_REL) {
-            emit(uinput_fd, EV_REL, ev.code, ev.value);
-            emit(uinput_fd, EV_SYN, SYN_REPORT, 0);
-        }
+    int ufd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+    if (ufd < 0) {
+        perror("Opening /dev/uinput");
+        return 3;
     }
 
-    // Cleanup
+    setup_uinput(ufd);
+
+    struct input_event e;
+    while (read(fd, &e, sizeof(e)) > 0) {
+        if (e.type == EV_KEY) {
+            if (e.code == BTN_EXTRA) e.code = KEY_8;
+            else if (e.code == BTN_SIDE) e.code = KEY_9;
+        }
+        write(ufd, &e, sizeof(e));
+    }
+
     ioctl(fd, EVIOCGRAB, 0);
-    ioctl(uinput_fd, UI_DEV_DESTROY);
+    ioctl(ufd, UI_DEV_DESTROY);
     close(fd);
-    close(uinput_fd);
+    close(ufd);
     return 0;
 }
